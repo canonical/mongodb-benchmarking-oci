1f7f01626a948713c5ce91409f01a80d22908ee9
Dmitry Ratushnyy <dmitry.ratushnyy@canonical.com>
2024-04-22 12:37:22 +0000

    Update ycsb scriptts

--- scripts/ycsb_load_data.sh
+++ scripts/ycsb_load_data.sh
@@ -5,21 +5,27 @@ ln -sf /usr/bin/python2.7 /usr/bin/python
 export PATH="/usr/lib/jvm/java-21-openjdk-amd64/bin/:/usr/share/maven/bin/:$PATH"
 
 # Default values
-RECORD_COUNT_DEFAULT=500000
-LOAD_THREADS_COUNT_DEFAULT=16
-MONGODB_URI_DEFAULT="mongodb://localhost:21017/admin?replicaSet=mongodb"
-WORKLOAD_DEFAULT="workloada"
+RECORD_COUNT=500000
+LOAD_THREADS_COUNT=16
+MONGODB_URI="mongodb://localhost:21017/admin?replicaSet=mongodb-k8s"
+WORKLOAD="workloada"
 
-# Read parameters or use defaults
-WORKLOAD=${1:-$WORKLOAD_DEFAULT}
-RECORD_COUNT=${2:-$RECORD_COUNT_DEFAULT}
-LOAD_THREADS_COUNT=${3:-$LOAD_THREADS_COUNT_DEFAULT}
-MONGODB_URI=${4:-$MONGODB_URI_DEFAULT}
+# Parse named arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -w|--workload) WORKLOAD="$2"; shift ;;
+        -rc|--record-count) RECORD_COUNT="$2"; shift ;;
+        -lt|--load-threads-count) LOAD_THREADS_COUNT="$2"; shift ;;
+        -mu|--mongodb-uri) MONGODB_URI="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
 
+# Run YCSB with specified parameters
 /var/load-testing/ycsb-0.17.0/bin/ycsb \
     load mongodb -s \
     -P "/var/load-testing/ycsb-0.17.0/workloads/$WORKLOAD" \
     -p recordcount=$RECORD_COUNT \
     -threads $LOAD_THREADS_COUNT \
     -p mongodb.url="$MONGODB_URI"
-    
